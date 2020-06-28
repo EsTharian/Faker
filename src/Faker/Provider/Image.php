81e1c4ce759ad8cb1b458a36b132a5afe923ec68
Muhammed Taha Ayan <tahaayan@gmail.com>
2020-06-28 16:52:10 +0300

    https://picsum.photos/ configuration

--- src/Faker/Provider/Image.php
+++ src/Faker/Provider/Image.php
@@ -3,52 +3,49 @@
 namespace Faker\Provider;
 
 /**
- * Depends on image generation from http://lorempixel.com/
+ * Depends on image generation from https://picsum.photos/
+ * So, there is no category this time.
  */
 class Image extends Base
 {
-    protected static $categories = array(
-        'abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife',
-        'fashion', 'people', 'nature', 'sports', 'technics', 'transport'
-    );
-
     /**
      * Generate the URL that will return a random image
      *
      * Set randomize to false to remove the random GET parameter at the end of the url.
      *
-     * @example 'http://lorempixel.com/640/480/?12345'
+     * @example 'https://picsum.photos/640/480.jpg'
      *
      * @param integer $width
      * @param integer $height
-     * @param string|null $category
      * @param bool $randomize
      * @param string|null $word
      * @param bool $gray
+     * @param integer|null $blur You can adjust the amount of blur by providing a number between 1 and 10.
+     * @param string|null $ext If you need a file ending, you can declare as `.jpg` or to get an image in the WebP format, you can declare as `.webp`.
      *
      * @return string
      */
-    public static function imageUrl($width = 640, $height = 480, $category = null, $randomize = true, $word = null, $gray = false)
+    public static function imageUrl($width = 640, $height = 480, $randomize = true, $word = null, $gray = false, $blur = null, $ext=null)
     {
-        $baseUrl = "https://lorempixel.com/";
-        $url = "{$width}/{$height}/";
+        $now = round((mt_rand() / mt_getrandmax() + microtime(true)) * 1000);
+
+        $baseUrl = "https://picsum.photos/";
+        $url = "{$width}/{$height}{$ext}";
+
+        $is_arg = false;
 
         if ($gray) {
-            $url = "gray/" . $url;
+            $url .= "?grayscale";
+            $is_arg = true;
         }
 
-        if ($category) {
-            if (!in_array($category, static::$categories)) {
-                throw new \InvalidArgumentException(sprintf('Unknown image category "%s"', $category));
-            }
-            $url .= "{$category}/";
-            if ($word) {
-                $url .= "{$word}/";
-            }
+        if ($blur) {
+            $url .= $is_arg ? "&blur={$blur}" : "?blur={$blur}";
+            $is_arg = true;
         }
 
         if ($randomize) {
-            $url .= '?' . static::randomNumber(5, true);
+            $url .= $is_arg ? "&random={$now}" : "?random={$now}";
         }
 
         return $baseUrl . $url;
@@ -61,7 +58,7 @@ class Image extends Base
      *
      * @example '/path/to/dir/13b73edae8443990be1aa8f1a483bc27.jpg'
      */
-    public static function image($dir = null, $width = 640, $height = 480, $category = null, $fullPath = true, $randomize = true, $word = null, $gray = false)
+    public static function image($dir = null, $width = 640, $height = 480, $fullPath = true, $randomize = true, $word = null, $gray = false, $blur = null, $ext=null)
     {
         $dir = is_null($dir) ? sys_get_temp_dir() : $dir; // GNU/Linux / OS X / Windows compatible
         // Validate directory path
@@ -72,10 +69,10 @@ class Image extends Base
         // Generate a random filename. Use the server address so that a file
         // generated at the same time on a different server won't have a collision.
         $name = md5(uniqid(empty($_SERVER['SERVER_ADDR']) ? '' : $_SERVER['SERVER_ADDR'], true));
-        $filename = $name .'.jpg';
+        $filename = $name . $ext ?? '.jpg';
         $filepath = $dir . DIRECTORY_SEPARATOR . $filename;
 
-        $url = static::imageUrl($width, $height, $category, $randomize, $word, $gray);
+        $url = static::imageUrl($width, $height, $randomize, $word, $gray, $blur = null, $ext=null);
 
         // save file
         if (function_exists('curl_exec')) {
